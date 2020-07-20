fc37a6c8033fd28251623d8d1481e55d9ccc71e1
Mudassir Khan <38735197+Muddi00seven@users.noreply.github.com>
2020-07-20 23:39:30 +0500

    UPDATED

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
 import React from 'react';
 import logo from './logo.svg';
 import './App.css';
+import Animation from './components/animation';
 
 function App() {
   return (
     <div className="App">
+      <Animation/>
     </div>
   );
 }
---
+++ src/components/animation.js
@@ -0,0 +1,13 @@
+import React,{useState} from 'react';
+import useWebAnimations, { pulse } from "@wellyshen/use-web-animations";
+
+
+const Animation = () => {
+    return (
+        <div>
+            <h1>HELLO WORLD</h1>
+        </div>
+    )
+}
+
+export default Animation
